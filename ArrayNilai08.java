5d964c76cd45c0327fe5b782d08752cfab9a7d36
Daffaazzi <azzidaffa@gmail.com>
2025-11-10 22:28:43 +0700

    memodifikasi menambahkan nilai akhirr.length

--- ArrayNilai08.java
+++ ArrayNilai08.java
@@ -3,8 +3,8 @@ public class ArrayNilai08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i <10; i++) {
-            System.out.print("Masukkan nilai akhir ke-"+i+" :");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
 
         }
@@ -13,6 +13,6 @@ public class ArrayNilai08 {
                 System.out.println("Nilai akhir ke-"+i+" adalah : "+nilaiAkhir[i]);
             
         }
-        
+
     }
 }
